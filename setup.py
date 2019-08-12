67042da2b86e777cb8ea053e141b763cc9a2a529
Houston2013 <jerome198246@gmail.com>
2019-08-12 12:50:37 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="Local Linear Forest",
+    version="0.0.1",
+    author="jingbo liu",
+    author_email="jingbo.liu2013@gmail.com",
+    description="A POC python implementation of local linear forest",
+    url="https://github.com/HoustonJ2013/LocalLinearForest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
